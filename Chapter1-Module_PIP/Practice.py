0381519dd4429c7420733f51352a91017887408b
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-08-18 12:41:31 +0530

    Importing the os for listing the file directoreis

--- Chapter1-Module_PIP/Practice.py
+++ Chapter1-Module_PIP/Practice.py
@@ -1,4 +1,9 @@
-import pyttsx3
-engine = pyttsx3.init()
-engine.say("Hey i am jarves how can i help you")
-engine.runAndWait()
+
+import os
+
+directory_path = '/rs'
+
+contents = os.listdir(directory_path)
+
+for item in contents:
+    print(item)
