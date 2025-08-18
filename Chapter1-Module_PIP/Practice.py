c1e9e1327fde6095beae6ee36d91dd3c8ff56de0
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-08-18 12:30:56 +0530

    Printing poem in python

---
+++ Chapter1-Module_PIP/Practice.py
@@ -0,0 +1,3 @@
+print("Jingle bells, jingle bellsJingle all the way.Oh! what fun it is to rideIn a one-horse open sleigh. Hey!Jingle bells, jingle bells,Jingle all the way;Oh! what fun it is to rideIn a one-horse open sleighA day or two agoI thought Id take a rideAnd soon, Miss Fanny BrightWas seated by my side,The horse was lean and lankMisfortune seemed his lotHe got into a drifted bankAnd then we got upsot")
+
+
