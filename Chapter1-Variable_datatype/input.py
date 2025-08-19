2f7f84fa86c4c3392fb1a5223288b2b3c155c768
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-08-19 10:39:03 +0530

    Inputs in python

---
+++ Chapter1-Variable_datatype/input.py
@@ -0,0 +1,16 @@
+# a = input("Enter Num 1:" )
+# b = input("Enter Num 2:" )
+# print("A is", a)
+# print("b is", b)
+# print("Sum is a + b is", a + b) #This code will do not sum the num because by  default input accept the stings so it will covert the number into string and concat it in to '12'
+# #output = Sum is a + b is 12
+
+
+
+# For sum the num we need to write int brfore input keyword
+a = int(input("Enter Num 1:" ))
+b = int(input("Enter Num 2:" ))
+print("A is", a)
+print("b is", b)
+print("Sum is a + b is", a + b)
+# #output =Sum is a + b is 3
