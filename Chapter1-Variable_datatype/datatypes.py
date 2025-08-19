09891f5f71dcda68f1d88bb97a6a50513407ab4d
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-08-19 09:49:02 +0530

    Datatypes in python

---
+++ Chapter1-Variable_datatype/datatypes.py
@@ -0,0 +1,11 @@
+# Data types in Python are classifications that categorize the type of values a variable can hold
+
+Number  = 1 # Number is an interger datatype
+
+big = 20.20  # big is a floating point number datatype
+
+Name  = "Abhay Anuskha Ratnakar"  # Name is string datatype
+
+isActive =  True # is active is boolean datatype
+
+errorss = None # errorss is a none data type 
