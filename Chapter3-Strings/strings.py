c02772b46e5cf51e748129c18c5da83d271469b5
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-08-20 09:54:33 +0530

    Some methods of Python

--- Chapter3-Strings/strings.py
+++ Chapter3-Strings/strings.py
@@ -1,23 +1,30 @@
-# String is a data in python
-# String is a sequance of character enclose in quotes 
+# # String is a data in python
+# # String is a sequance of character enclose in quotes 
 
-# String declaration
+# # String declaration
 # name = "Abhay"
 # print(name)
 
 
 
-# # String slicing
+# # # String slicing
 # name = "Abhay"
 # slic = name[0:2]
 # print(slic)
 
-# # Negative index
+# # # Negative index
 # slic = name[-4:-1]
 # print(slic)
 
  
-# Slicing With Skip value
-sk = "abcdefghinkjbdbckwbkcbwkcbbwckhkewkwbcbwkcbbcnosi"
-ans = sk[1:10:5]
-print(ans) 
+# # Slicing With Skip value
+# sk = "abcdefghinkjbdbckwbkcbwkcbbwckhkewkwbcbwkcbbcnosi"
+# ans = sk[1:10:5]
+# print(ans) 
+
+
+# Stings methods
+name = "Abhay"
+print(len(name))
+print(name.endswith("y"))
+print(name.startswith("y"))
