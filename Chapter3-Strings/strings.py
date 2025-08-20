2ecae49c2b2fb47c902c861fccd41294ecfd14fc
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-08-20 10:03:45 +0530

    .

--- Chapter3-Strings/strings.py
+++ Chapter3-Strings/strings.py
@@ -2,25 +2,25 @@
 # # String is a sequance of character enclose in quotes 
 
 # # String declaration
-# name = "Abhay"
-# print(name)
+name = "Abhay"
+print(name)
 
 
 
 # # # String slicing
-# name = "Abhay"
-# slic = name[0:2]
-# print(slic)
+name = "Abhay"
+slic = name[0:2]
+print(slic)
 
 # # # Negative index
-# slic = name[-4:-1]
-# print(slic)
+slic = name[-4:-1]
+print(slic)
 
  
 # # Slicing With Skip value
-# sk = "abcdefghinkjbdbckwbkcbwkcbbwckhkewkwbcbwkcbbcnosi"
-# ans = sk[1:10:5]
-# print(ans) 
+sk = "abcdefghinkjbdbckwbkcbwkcbbwckhkewkwbcbwkcbbcnosi"
+ans = sk[1:10:5]
+print(ans) 
 
 
 # Stings methods
