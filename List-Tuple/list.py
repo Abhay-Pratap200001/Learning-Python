a73a63976a367e4483da447fc2ec941ebe792b14
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-08-21 22:46:31 +0530

    Complete the leaarning list in python

---
+++ List-Tuple/list.py
@@ -0,0 +1,38 @@
+# Printing all data types 
+lsittt = ["Mango", "Apple", "Bnana", 20, 20.0, True, ]
+print(lsittt)
+
+# Print a mutabble we can change it
+lsittt[1] = "Pinapple"  
+print(lsittt)
+
+List methode
+lsittt = ["Mango", "Apple", "Bnana", 20, 20.0, True, ]
+lsittt.append("Lichi") add value in last index
+print(lsittt)
+
+
+num = [2900,345,947,20,21,]
+num.sort()
+print(num) sort the value from smaller to greater
+
+
+lsittt = ["Mango", "Apple", "Bnana", 20, 20.0, True, ]
+lsittt.reverse()
+print(lsittt) reverse the list from last index to first index
+
+lsittt = ["Mango", "Apple", "Bnana", 20, 20.0, True, ]
+lsittt.insert(1, "dragon fruite")
+print(lsittt) insert middle of the list according to need
+
+
+lsittt = ["Mango", "Apple", "Bnana", 20, 20.0, True, ]
+lsittt.pop(2) remove list based on index
+print(lsittt)
+
+
+
+lsittt = ["Mango", "Apple", "Bnana",3, 20, 20.0, True, ]
+lsittt.remove(3)
+print(lsittt) remove value what you enter
+
