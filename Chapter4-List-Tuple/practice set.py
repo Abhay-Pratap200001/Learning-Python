ea02b2c5c2f1125efc9b6300fd73df31af0cb4dd
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-08-22 22:57:40 +0530

    Practice sets of tuple and lists

---
+++ Chapter4-List-Tuple/practice set.py
@@ -0,0 +1,54 @@
+student = []
+
+f1 = input("Enter fruit name: ")
+student.append(f1)
+f2 = input("Enter fruit name: ")
+Fruits.append(f2)
+f3 = input("Enter fruit name: ")
+Fruits.append(f3)
+f4 = input("Enter fruit name: ")
+
+Fruits.append(f4)
+f5 = input("Enter fruit name: ")
+Fruits.append(f5)
+f6 = input("Enter fruit name: ")
+Fruits.append(f6)
+f7 = input("Enter fruit name: ")
+Fruits.append(f7)
+f8 = input("Enter fruit name: ")
+Fruits.append(f8)
+print(Fruits)
+output = [' apple', ' mango', ' mango', 'bnana', 'dragonfruite', 'avacrado', 'lichi', 'blueberry']
+
+
+student = []
+
+# f1 = int(input("Enter student name: "))
+student.append(f1)
+f2 = int(input("Enter student name: "))
+student.append(f2)
+f3 = int(input("Enter student name: "))
+student.append(f3)
+f4 = int(input("Enter student name: "))
+student.append(f4)
+f5 = int(input("Enter student name: "))
+student.append(f5)
+f6 = int(input("Enter student name: "))
+student.append(f6)
+f7 = int(input("Enter student name: "))
+student.append(f7)
+f8 = int(input("Enter student name: "))
+student.append(f7)
+
+student.sort()
+print(student)
+
+
+
+tuple practice
+
+Num = [33,555,67,77,854,34,20]
+print(sum(Num))
+
+Tup = (20,20,20,2,2,0,42,42,42)
+print(Tup.count(20))
