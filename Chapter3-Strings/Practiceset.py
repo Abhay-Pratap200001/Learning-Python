d1cc969d5945e44440d51fee5aceafc83e7eb586
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-08-22 22:36:49 +0530

    Complete the string practice set

---
+++ Chapter3-Strings/Practiceset.py
@@ -0,0 +1,18 @@
+# name = input("Enter you name :")
+# print(f"My Name is {name}")
+
+
+# letter = ''' Hello <|Name|>,
+#            you are selected on!
+#            <|Date|>'''
+
+# print(letter.replace("<|Name|>", "Abhay").replace("<|Date|>", "20 December"))
+
+
+# name = "Abhay you are  selected"
+# print(name.find("  "))
+
+# name = "Abhay you are  selected"
+# print(name.replace("  ", " "))
+
+
