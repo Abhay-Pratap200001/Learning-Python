1a70b790d22a9ef68bb03e21b244cef04238225c
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-08-22 22:36:24 +0530

    Complete the sets

---
+++ Chapter-5-Dictionary & sets/sets.py
@@ -0,0 +1,9 @@
+# In Python, a set is an unordered collection of unique and immutable elements. Sets are defined using curly braces {} or the set() constructo
+s = {1,2,3,4,5,6}
+
+s.add(20)
+
+# use to add more values but can add dublicate
+print(s, type(s))
+s.remove(5)
+print(s)
