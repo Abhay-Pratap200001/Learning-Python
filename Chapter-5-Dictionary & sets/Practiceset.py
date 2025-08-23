cf03bf0323a0e588d7472e4316e5815e413be68e
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-08-23 13:06:53 +0530

    Practice set finished of Dictiinary and sets with learning some more methods

---
+++ Chapter-5-Dictionary & sets/Practiceset.py
@@ -0,0 +1,133 @@
+words = {
+    "kam":"work",
+    "janam":"birth",
+    "Naukari":"job",
+    "pyar":"love"
+}
+
+word = input("Enter the world you want meaning od: ")
+print(words[word])
+s = set()
+
+num = input("Enter num 1: ")
+s.add(int(num))
+num = input("Enter num 2: ")
+s.add(int(num))
+num = input("Enter num 3: ")
+s.add(int(num))
+num = input("Enter num 4: ")
+s.add(int(num))
+num = input("Enter num 5: ")
+s.add(int(num))
+num = input("Enter num 6: ")
+s.add(int(num))
+num = input("Enter num 7: ")
+s.add(int(num))
+num = input("Enter num 8: ")
+s.add(int(num))
+
+print(s)
+
+
+s = set()
+
+num = input("Enter num 1: ")
+s.add(int(num))
+num = input("Enter num 2: ")
+s.add(int(num))
+num = input("Enter num 3: ")
+s.add(int(num))
+num = input("Enter num 4: ")
+s.add(int(num))
+num = input("Enter num 5: ")
+s.add(int(num))
+num = input("Enter num 6: ")
+s.add(int(num))
+num = input("Enter num 7: ")
+s.add(int(num))
+num = input("Enter num 8: ")
+s.add(int(num))
+num = input("Enter num 1: ")
+s.add(int(num))
+num = input("Enter num 2: ")
+s.add(int(num))
+num = input("Enter num 3: ")
+s.add(int(num))
+num = input("Enter num 4: ")
+s.add(int(num))
+num = input("Enter num 5: ")
+s.add(int(num))
+num = input("Enter num 6: ")
+s.add(int(num))
+num = input("Enter num 7: ")
+s.add(int(num))
+num = input("Enter num 8: ")
+s.add(int(num))
+num = input("Enter num 8: ")
+s.add(int(num))
+num = input("Enter num 8: ")
+s.add(int(num))
+
+
+num = input("Enter num 1: ")
+s.add((num))
+num = input("Enter num 2: ")
+s.add((num))
+num = input("Enter num 3: ")
+s.add((num))
+num = input("Enter num 4: ")
+s.add((num))
+num = input("Enter num 5: ")
+s.add((num))
+num = input("Enter num 6: ")
+s.add((num))
+num = input("Enter num 7: ")
+s.add((num))
+num = input("Enter num 8: ")
+s.add((num))
+num = input("Enter num 1: ")
+s.add((num))
+num = input("Enter num 2: ")
+s.add((num))
+num = input("Enter num 3: ")
+s.add((num))
+num = input("Enter num 4: ")
+s.add((num))
+num = input("Enter num 5: ")
+s.add((num))
+num = input("Enter num 6: ")
+s.add((num))
+num = input("Enter num 7: ")
+s.add((num))
+num = input("Enter num 8: ")
+s.add((num))
+num = input("Enter num 8: ")
+s.add((num))
+num = input("Enter num 8: ")
+s.add((num))
+
+print(type(s),s)
+
+dictt={}
+name = input('Enter fiends name:')
+programinglanguage = input('Enter fiends progarming langyahe:')
+dictt.update({name: programinglanguage})
+
+
+name = input('Enter fiends name:')
+programinglanguage = input('Enter fiends progarming langyahe:')
+dictt.update({name: programinglanguage})
+
+
+name = input('Enter fiends name:')
+programinglanguage = input('Enter fiends progarming langyahe:')
+dictt.update({name: programinglanguage})
+
+
+name = input('Enter fiends name:')
+programinglanguage = input('Enter fiends progarming langyahe:')
+dictt.update({name: programinglanguage})
+
+print(dictt)
+
+
--- Chapter-5-Dictionary & sets/sets.py
+++ Chapter-5-Dictionary & sets/sets.py
@@ -3,7 +3,30 @@ s = {1,2,3,4,5,6}
 
 s.add(20)
 
-# use to add more values but can add dublicate
+# # use to add more values but can add dublicate
 print(s, type(s))
 s.remove(5)
 print(s)
+
+
+ans=s.clear()
+print(ans)
+
+
+
+s.__len__()
+print(s)
+
+# take both of values and do not repeated
+s1 = {1,45,6}
+s2 = {7,8,1,78}
+print(s1.union(s2))
+
+# in intersection take from both of common values
+print(s1.intersection(s2))
+
+
+print({1,45,6}.issubset(s1))
+print({1,45,6}.issuperset(s1))
+
+
