bff23b5a22409de984eafef4fed2b71e5ec5ba5b
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2025-08-26 14:42:04 +0530

    Complete the practicing List and tuple in python

---
+++ Chapter4-List-Tuple/Practice_List_Tuples.py
@@ -0,0 +1,41 @@
+# Q1: Create list from 1–10 & find sum.
+# number = [1,2,3,4,5,6,7,8,9,20]
+# print("Sum: ", sum(number))
+
+
+
+
+# Q2: Find largest number without max().
+# number = [1,2,3,4,5,6,7,8,9,20]
+# largest_num = number[0]
+
+# for num in number:
+#     if num > largest_num:
+#         largest_num = num
+# print("Largest nun is: ", largest_num)
+
+
+
+
+# Q3: Convert list to tuple & check type.
+# number = [1,2,3,4,5,6,7,8,9,20]
+# cov_tuple = tuple(number)
+# print(cov_tuple)
+# print(type(cov_tuple))
+
+
+
+
+# Q4: Remove duplicates from list.
+# number = [1,1,1,3,4,5,8,2,3,4,5,6,7,8,9,20]
+# uniqlist = list(set(number))
+# print(uniqlist)
+
+
+
+
+# Q5: Access 3rd element in tuple.
+# tup = (10, 30, 20, 20, 60, 70)
+# print("3rd element:",tup[2])
+
+
